337a501857e93f657d3e6cb5bfff44889b53aea6
Bruno Vieira <bruno.silvavieira@gmail.com>
2018-05-10 08:15:35 -0300

    Criação do projeto

---
+++ src/main/java/com/brunovieira/sellersys/SellersysApplication.java
@@ -0,0 +1,12 @@
+package com.brunovieira.sellersys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellersysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellersysApplication.class, args);
+	}
+}
---
+++ src/test/java/com/brunovieira/sellersys/SellersysApplicationTests.java
@@ -0,0 +1,16 @@
+package com.brunovieira.sellersys;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SellersysApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
