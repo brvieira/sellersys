894469ab277294b65e311adf614ffab4c47bee87
Bruno Vieira <bruno.silvavieira@gmail.com>
2018-05-11 19:24:37 -0300

    Criação da classe domain - Categoria

--- src/main/java/com/brunovieira/sellersys/resources/CategoriaResource.java
+++ src/main/java/com/brunovieira/sellersys/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.brunovieira.sellersys.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.brunovieira.sellersys.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> listar() {
+		List<Categoria> lista = new ArrayList<>();
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
